fc9a0b711b05dca2ac8533b99bd0a650bf7bcfbb
Ubuntu <ubuntu@ip-172-26-13-33.us-west-2.compute.internal>
2025-02-11 22:44:13 +0000

    Update bot.blade.php and clean up cache files

--- storage/framework/views/7c6155fcc60ca2681971f6e0118ddb56.php
+++
@@ -1,5 +0,0 @@
-<button <?php echo e($attributes->merge(['type' => 'submit', 'class' => 'border border-[#173F74] py-2 px-5 text-center bg-[#173F74] rounded-full text-white text-[16px] hover:bg-[#1f559c] font-semibold'])); ?>>
-    <?php echo e($slot); ?>
-
-</button>
-<?php /**PATH /var/www/resources/views/components/primary-button.blade.php ENDPATH**/ ?>
